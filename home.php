057746661f6bd4aa2c28f442a565b6ff2fbdca4b
alexandre-pinon <alexandre.pinon@epitech.eu>
2020-10-05 17:16:31 +0200

    Apply Form

--- home.php
+++ home.php
@@ -5,7 +5,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link href="custom.css" rel="stylesheet"> 
-    <title>Document</title>
+    <title>THE Job Board</title>
 </head>
     <body>
         <?php
@@ -32,18 +32,24 @@
                 while($row = $result->fetch_assoc()) { ?>
 
                     <div class="row container">
-                        <div class="card hoverable grey darken-3 white-text carousel-item">
+                        <div class="card hoverable grey darken-3 white-text">
                             <div class="card-image">
                                 <img src="company_backgrounds/<?php echo $row["background"]; ?>" class="job-image">
-                                <img src="company_logos/<?php echo $row["logo"]; ?>" class="logo-image">
                                 <a class="activator btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+                                <a class="btn-floating halfway-fab company-logo"><img src="company_logos/<?php echo $row["logo"]; ?>"></a>
                             </div>
-                            <div class="card-content">
+                            <div class="card-content short-description">
                                 <span class="card-title"><?php echo $row["title"]; ?></span>
-                                <p class="test"><?php echo $row["description"]; ?></p>
+                                <p class=""><?php echo $row["description"]; ?></p>
+                            </div>
+                            <div class="card-action center">
+                                <a class="modal-trigger" href="#apply_form">Apply now !</a>
                             </div>
                             <div class="card-reveal grey lighten-3 grey-text text-darken-3">
-                                <span class="card-title"><?php echo $row["title"]; ?></span>
+                                <span class="card-title close-reveal">
+                                    <?php echo $row["title"]; ?>
+                                    <i class="material-icons right">close</i>
+                                </span>
                                 <p>
                                     Type of contract : <?php echo $row["contract_type"]; ?>,
                                     Localisation : <?php echo $row["localisation"]; ?>
@@ -59,6 +65,71 @@
             }
             $conn->close();
         ?>
+        <div id="apply_form" class="modal grey lighten-3 grey-text text-darken-3">
+            <div class="modal-content">
+                <a class="modal-close center grey lighten-3 grey-text text-darken-3">
+                    <h4>Apply Form<i class="material-icons right">close</i></h4>
+                </a>
+                <div class="row">
+                    <form class="col s12" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+                        <div class="row">
+                            <div class="input-field col s6">
+                                <i class="material-icons prefix">account_circle</i>
+                                <input id="first_name" type="text" class="validate">
+                                <label for="first_name">First Name</label>
+                            </div>
+                            <div class="input-field col s6">
+                                <input id="last_name" type="text" class="validate">
+                                <label for="last_name">Last Name</label>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="input-field col s12">
+                                <i class="material-icons prefix">lock</i>
+                                <input id="password" type="password" class="validate">
+                                <label for="password">Password</label>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="input-field col s12">
+                                <i class="material-icons prefix">email</i>
+                                <input id="email" type="email" class="validate">
+                                <label for="email">Email</label>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="input-field col s12">
+                                <i class="material-icons prefix">phone</i>
+                                <input id="phone" type="text" class="validate">
+                                <label for="phone">Phone</label>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="input-field col s12">
+                                <i class="material-icons prefix">edit</i>
+                                <textarea id="textarea" class="materialize-textarea" data-length="1000"></textarea>
+                                <label for="textarea">Message</label>
+                            </div>
+                        </div>
+                        <div class="file-field input-field">
+                            <div class="btn">
+                                <span>CV</span>
+                                <input type="file">
+                            </div>
+                            <div class="file-path-wrapper">
+                                <input class="file-path validate" type="text">
+                            </div>
+                        </div>
+                        <div class="row center">
+                            <button class="btn waves-effect waves-light" type="submit" name="action">
+                                Apply
+                                <i class="material-icons right">send</i>
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
         
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
--- main.js
+++ main.js
@@ -1,8 +1,22 @@
 $(document).ready(function(){
+    $('.modal').modal();
     $('.carousel').carousel();
-    $(".test").dotdotdot({
-        height: 50,
+    $('textarea#textarea').characterCounter();
+    $('.short-description').dotdotdot({
+        height: 165,
         fallbackToLetter: true,
         watch: true,
-	});
+    });
+    $('a.activator').click(function () {
+        $(this).parent().parent().animate(
+            {height: '500px'},
+            'slow'
+        );
+    });
+    $('.close-reveal').click(function () {
+        $(this).parent().parent().animate(
+            {height: '279.2px'},
+            'slow'
+        );
+    });
 });
